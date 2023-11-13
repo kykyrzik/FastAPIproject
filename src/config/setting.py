c5daebb2e827bdc42b983a135e40b9bb77a90a5c
Nikolay Ivanyukov <ivanukov78@gmail.com>
2023-11-13 18:40:16 +0400

    refactor setting and create url db

--- src/config/setting.py
+++ src/config/setting.py
@@ -1,9 +1,11 @@
-import secrets
+from typing import Any
 from functools import lru_cache
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import field_validator, Field
 
 
 class Settings(BaseSettings):
+
     model_config = SettingsConfigDict(
         env_file="./.env",
         env_file_encoding="utf-8",
@@ -12,23 +14,20 @@ class Settings(BaseSettings):
     DB_HOST: str
     DB_USER: str
     DB_PORT: int
-    DB_URL: str
     DB_PASS: str
     DB_NAME: str
+    DB_URL: str = Field(...)
 
-    SECRECT_KEY: str = secrets.token_hex(32)
-
-    @property
-    def get_db(self) -> str:
-        return self.DB_URL.format(
-            DB_USER=self.DB_USER,
-            DB_PASS=self.DB_PASS,
-            DB_HOST=self.DB_HOST,
-            DB_PORT=self.DB_PORT,
-            DB_NAME=self.DB_NAME,
-        )
+    @field_validator('DB_URL')
+    @classmethod
+    def assemble_db_url(cls, v: str, values: Any) -> str:
+        return v.format(DB_USER=values.data.get('DB_USER'),
+                        DB_PASS=values.data.get('DB_PASS'),
+                        DB_HOST=values.data.get('DB_HOST'),
+                        DB_PORT=values.data.get('DB_PORT'),
+                        DB_NAME=values.data.get('DB_NAME'))
 
 
 @lru_cache(typed=True)
 def load_settings() -> Settings:
-    return Settings()
+    return Settings()  # type: ignore
---
+++ src/service/database/repositories/base.py
@@ -0,0 +1,6 @@
+from typing import Protocol
+from abc import ABC
+
+
+class BaseCRUD(ABC, Protocol):
+    pass
