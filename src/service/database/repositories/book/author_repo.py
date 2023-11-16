af440c612b6a02f2c00d7e4ce12b79bc1c199891
Nikolay Ivanyukov <ivanukov78@gmail.com>
2023-11-16 19:29:54 +0400

    create author_repo

---
+++ src/service/database/repositories/book/author_repo.py
@@ -0,0 +1,28 @@
+from typing import List, Optional
+
+from src.service.database.repositories.base import CRUDBase
+from src.service.database.models.book.author import Author
+from src.service.database.schemas.book.author import AuthorsDTO
+
+
+class AuthorRepositories(CRUDBase):
+    model = Author
+
+    async def get_author(self, author_id: int) -> Optional[str]:
+        return await self._get(field=self.model, value=author_id)
+
+    async def get_list_authors(self, limit: int = 20) -> Optional[List[str]]:
+        return await self._get_list(limit)
+
+    async def create_author(self, data: AuthorsDTO) -> AuthorsDTO:
+        data = data.__dict__
+        return await self._create(data)
+
+    async def update_author(self, data: AuthorsDTO, author_id: Optional[int] = None) -> AuthorsDTO:
+        id_author = data.__dict__.pop("id")
+        data = data.__dict__
+        if not author_id:
+            return await self._update(field=self.model,
+                                      value=id_author,
+                                      data=data)
+        return await self._update(field=self.model, value=author_id, data=data)
