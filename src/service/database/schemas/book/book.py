bbc439299979606db7537c376ebc83098a30a794
Nikolay Ivanyukov <ivanukov78@gmail.com>
2023-11-17 13:25:32 +0400

    refactor book schemas

--- src/service/database/schemas/book/book.py
+++ src/service/database/schemas/book/book.py
@@ -6,17 +6,20 @@ from pydantic import BaseModel
 from sqlalchemy.types import BLOB
 
 
-class BookDTO(BaseModel):
-    ISBN: str
+class UpdateBookDTO(BaseModel):
     name_book: str
     author_id: int
     genre_id: int
+    unit_price: Decimal
+    amount_book: int
+
+
+class BookDTO(UpdateBookDTO):
+    ISBN: str
     publication_date: Optional[datetime] = None
     book_circulation: Optional[int] = None
-    unit_price: Decimal
     weight: Optional[int]
     rars_id: int
     number_of_pages: int
     cover_type_id: int
-    images: Optional[BLOB]
-    amount_book: int
+    images: Optional[BLOB] = None
